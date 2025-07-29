56eac1537aea464f3e550f2bdb352ab47ae170dd
Narin Suksawang <s6704062611107@email.kmutnb.ac.th>
2025-07-29 14:50:42 +0700

    Update App.java

--- L4/MatrixMultiplication/src/App.java
+++ L4/MatrixMultiplication/src/App.java
@@ -1,22 +1,44 @@
 import java.util.*;
 class MatrixMultiplication {
     int A[],B[];
-    int i,k,iL;
-    public MatrixMultiplication(int i, int k,int A[],int B[], int iL) {
+    int i,kR,iL;
+    int NewMatrix[];
+    public MatrixMultiplication(int A[],int B[], int iL,int kR) {
         this.A = A;
         this.B = B;
-        this.i = i;
-        this.k = k;
+        this.i = iL*kR;
+        this.NewMatrix  = new int[i+1];
+        System.out.println(i);
+        this.kR = kR;
         this.iL = iL;
+    }
+    void ShowMatrix() {
+        int index = 1;
+        while (index <=  i) {
+            System.out.print(NewMatrix[index]);
+            if (index % kR  == 0) {
+                System.out.print("\n");
+                
+            } else  {
+                System.out.print(" ");
+            }
+            index++;
+        }
     }
     void MatrixGen() {
         int index = 0;
         int index2 = 0;
-        while (index < i) {
+        int index3 = 1;
+        int index4 = 0;
+        while (index < iL) {
             index2 = 0;
-            while (index2 < k) {
-                
+            while (index2 < kR) {
+                NewMatrix[index3] = A[index4] * B[index2];
+                NewMatrix[index3] += A[index4+1] * B[index2+kR];
+                index2++;
+                index3++;
             }
+            index4+=2;
             index++;
         }
     }
@@ -38,5 +60,8 @@ public class App {
         for (int i = 0; i < kL*kR;i++) {
             B[i] = input.nextInt();
         }
+        MatrixMultiplication Martrix = new MatrixMultiplication(A,B,iL,kR);
+        Martrix.MatrixGen();
+        Martrix.ShowMatrix();
     }
 }
